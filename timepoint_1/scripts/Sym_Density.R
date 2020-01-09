408ebffb5be90a1112d842ee22cbdf2475cdc3b9
Hollie Putnam <hollieputnam@gmail.com>
2020-01-08 19:35:36 -1000

    updating cell density counts and script

--- timepoint_1/scripts/Sym_Density.R
+++ timepoint_1/scripts/Sym_Density.R
@@ -9,35 +9,59 @@ rm(list=ls()) #clears workspace
 
 ## install packages if you dont already have them in your library
 #if ("devtools" %in% rownames(installed.packages()) == 'FALSE') install.packages('devtools') 
+library(tidyr)
+library(dplyr)
+library(plotrix)
+library(ggplot2)
 
-Data <- read.csv("RAnalysis/Data/Sym_Counts_Data.csv")
-Meta <- read.csv("RAnalysis/Data/Sample_Metadata.csv")
-Surface.Area <- read.csv("RAnalysis/Data/E5surface_area.csv") 
-Data <-merge(Data, Meta, by="Tube.ID")
-Data <- merge(Data, Surface.Area,  by="Sample.ID")
+Data <- read.csv("data/1_Sym_Counts_Data.csv")
+Meta <- read.csv("data/1_Sample_Info.csv")
+Data <-merge(Data, Meta, by="Plug.Number")
 
-Data$avg.cells <- rowMeans(Data[,4:9])
-Data$avg.cells[2] <- rowMeans(Data[2,4:7])
+#calculate surface area standard curve
+wax.data <- read.csv("data/1_Wax_dipping.csv", header=TRUE)
+wax.data$delta.mass.g <- wax.data$weight2.g-wax.data$weight1.g
+stnds <- subset(wax.data, Sample=="Standard")
+stnds <- stnds[-1,]
+stnds$rad <- stnds$Diameter/2
+stnds$surface.area.cm2 <- 4*pi*(stnds$rad)^2
+stnd.curve <- lm(surface.area.cm2~delta.mass.g, data=stnds)
+plot(surface.area.cm2~delta.mass.g, data=stnds)
+
+stnd.curve$coefficients
+summary(stnd.curve)$r.squared
+
+#Calculate surface area
+smpls <- subset(wax.data, Sample=="Coral")
+smpls$surface.area.cm2 <- stnd.curve$coefficients[2] * smpls$delta.mass.g + stnd.curve$coefficients[1]
+
+range(smpls$surface.area.cm2)
+range(stnds$surface.area.cm2)
+
+Data <- merge(Data, smpls, by="Plug.Number")
+
+#Load homogenate volume
+homog.vol <- read.csv("data/1_homogenate_vols.csv", header=TRUE)
+
+Data <- merge(Data, homog.vol, by="Plug.Number")
+
+Data$avg.cells <- rowMeans(Data[,3:8])
+#Data$avg.cells[2] <- rowMeans(Data[2,4:7])
 #Concentration = Number of cells x 10,000 / Number of squares
 Data$cells.ml <- Data$avg.cells * 10000 /Data$Squares.Counted
-Data$cells <- Data$cells.ml * Data$Homog.Vol.ml.x
-Data$cells.cm2 <- Data$cells/Data$SA.cm2
+Data$cells <- Data$cells.ml * Data$homog_vol_ml
+Data$cells.cm2 <- Data$cells/Data$surface.area.cm2
 Data$cells.106.cm2 <- Data$cells.cm2/10^6
+Data <- subset(Data, Species!="BK")
+plot(Data$cells.106.cm2 ~Data$Plug.Number, las=2)
 
-Data$group <- paste0(Data$Species,"_",Data$Site)
 
-myColors <- c("grey", "orange", "orange", "cyan", "cyan", "cyan", "green", "green", "green" )
+Data%>%
+  group_by(Species, Site)%>%
+  summarise(mean.value = mean(cells.106.cm2), se = std.error(cells.106.cm2)) %>%
+  ggplot(aes(x = Site, y = mean.value, group = Species, color = Species))+
+  geom_point(size = 3)+
+  geom_errorbar(aes(x = Site, ymin = mean.value-se, ymax = mean.value+se), width = 0.5)+
+  facet_grid(~Species, scales = "free_y")
 
-pdf("RAnalysis/Output/Sym_Density.pdf")
-#par(mfrow=c(1,2))
-boxplot(Data$cells.106.cm2 ~ Data$group, las=2, ylab=expression(paste("Cells x 10^6 cm-"^"2")),col=myColors, xaxt='n', xlab="") 
-legend("topleft", legend = c("Garden A.pulchra","A.pulchra", "Poc.meandrina", "Por.lutea" ) , 
-       col = c("grey","orange", "cyan","green" ) , bty = "n", pch=20 , pt.cex = 3, cex = 1, horiz = FALSE, inset = c(0.03, 0.1))
-dev.off()
 
-jpeg("RAnalysis/Output/Sym_Density.jpg")
-#par(mfrow=c(1,2))
-boxplot(Data$cells.106.cm2 ~ Data$group, las=2, ylab=expression(paste("Cells x 10^6 cm-"^"2")),col=myColors, xaxt='n', xlab="") 
-legend("topleft", legend = c("Garden A.pulchra","A.pulchra", "Poc.meandrina", "Por.lutea" ) , 
-       col = c("grey","orange", "cyan","green" ) , bty = "n", pch=20 , pt.cex = 3, cex = 1, horiz = FALSE, inset = c(0.03, 0.1))
-dev.off()
