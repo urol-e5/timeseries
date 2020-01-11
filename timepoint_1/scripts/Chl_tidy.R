ecc5cdb4f9b74387c9f0cca4b1d90d1198698a44
Ross Cunning <ross.cunning@gmail.com>
2020-01-10 15:25:04 -1000

    up through chla chlc formulae

--- timepoint_1/scripts/Chl_tidy.R
+++ timepoint_1/scripts/Chl_tidy.R
@@ -11,14 +11,43 @@ read_chl <- function(file) {
     unite("well", c(row, col), sep = "")
 }
 
-# List files with cholorophyll data
-files <- list.files(path = "data/1_Chl", pattern = "*.csv", full.names = TRUE)
+# List chlorophyll data files
+chl_path = "data/1_Chl/"                                                 # Path to chl data directory
+all_chl_files <- list.files(path = chl_path, pattern = "*.csv")          # List all files in directory
+chl_platemaps <- list.files(path = chl_path, pattern = "platemap")       # List platemap files
+chl_data_files <- setdiff(all_chl_files, chl_platemaps)                  # List absorbance data files
 
 # Read in all files into tibble
-df <- tibble(file = files) %>%
-  mutate(chl_data = map(file, ~ read_chl(.)))
+df <- tibble(file = chl_data_files) %>%
+  #filter(chl_data_files == "1_20200106_Chl.csv") %>%
+  mutate(platemap = map(file, ~ read_csv(paste0(chl_path, tools::file_path_sans_ext(.), "_platemap.csv"))),
+         chl_data = map(file, ~ read_chl(paste0(chl_path, .))))
 
-  
-df$chl_data[[1]]
-  
+# Merge platemap and data for each plate
+df <- df %>%
+  mutate(merged = map2(platemap, chl_data, ~ right_join(.x, .y)))
+
+
+####
+
+# average all technical replicates for each colony/wavelength, including all acetone blanks together
+df <- df %>%
+  unnest(merged) %>%
+  filter(!is.na(colony_id)) %>%                         # remove empty wells (colony_id is NA)
+  group_by(file, colony_id, wavelength) %>%
+  summarise(n = n(), mean_abs = mean(absorbance)) %>%
+  spread(wavelength, mean_abs)
+
+# get the acetone blank 750 absorbace for each file (i.e., plate), and subtract from 630 and 663 values for each sample
+df <- df %>%
+  group_by(file) %>%
+  mutate(blank750 = `750`[colony_id == "BK"]) %>%
+  ungroup() %>%
+  mutate(adj630 = `630` - blank750,
+         adj663 = `663` - blank750)
+
+# calculate chla and chlc2 values based on equations from Jeffrey and Humphrey 1975
+df <- df %>%
+  mutate(chla = 11.43 * adj663 - 0.64 * adj630,
+        chlc2 = 27.09 * adj630 - 3.63 * adj663)
 
