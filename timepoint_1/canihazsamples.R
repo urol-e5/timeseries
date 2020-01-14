fb2f460b5963c3d35a12ed15164aa8f38dc35fc8
Ross Cunning <ross.cunning@gmail.com>
2020-01-13 14:59:25 -1000

    sample cross-checker

---
+++ timepoint_1/canihazsamples.R
@@ -0,0 +1,59 @@
+
+# Which samples are represented in which datasets?
+coral_metadata <- read_csv("../metadata/coral_metadata.csv") %>% 
+  pull(colony_id)
+
+homog_vols <- read_csv("data/1_homogenate_vols.csv") %>% 
+  pull(colony_id)
+
+surface_area <- read_csv("data/1_Wax_dipping.csv") %>% 
+  pull(colony_id)
+
+chl <- list.files("data/1_Chl/", "platemap", full.names = TRUE) %>%
+  map_df(read_csv) %>%
+  pull(colony_id)
+
+afdw <- read_csv("data/1_Biomass.csv") %>%
+  drop_na() %>%
+  pull(colony_id)
+
+molec <- read_csv("data/1_DNARNAShield_sample_metadata.csv") %>%
+  distinct(colony_id) %>%
+  pull()
+
+picurve <- list.files("data/1_PICurves/", "*.csv") %>%
+  gsub("_.*.csv", "", .) %>%
+  subset(!grepl("BK", .))
+
+sym_counts <- read_csv("data/1_Sym_Counts_Data.csv") %>%
+  pull(colony_id)
+
+
+#####################################
+
+diffs <- function(x, y) {
+  list(
+    setdiff(x, y),
+    setdiff(y, x)
+  )
+}
+
+# Missing from homogenate volumes
+diffs(coral_metadata, homog_vols)
+
+# Missing from surface area
+diffs(coral_metadata, surface_area)
+
+# Missing from chlorophyll
+diffs(coral_metadata, chl)
+
+# Missing from surface area
+diffs(coral_metadata, surface_area)
+
+# Missing from ash free dry weight
+diffs(coral_metadata, afdw)
+
+# Missing from DNA/RNA shield tubes
+diffs(coral_metadata, molec)
+
+
