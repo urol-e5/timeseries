38d8db83bbf098837f9a79fab6603f246f34ea95
daniellembecker <danielle_becker@uri.edu>
2022-03-08 08:28:09 -1000

    added Jan sym counts to jan 2022 data folder that were completd on 0308

--- timepoint_1/1_trip_info.md
+++
@@ -1,7 +0,0 @@
-# Dates
-January 1-14, 2020
-
-# Participants
-Putnam, Strand, Conetta, Eirin-Lopez, Wong, Rose, Cunning
-
-# Trip notes
--- timepoint_3/scripts/pH_Tris_Curve.R
+++
@@ -1,9 +0,0 @@
-setwd("~/MyProjects/urol-e5/timeseries/timepoint_2/tris_calibrations") #set working directory
-
-Calib.Data <-read.table("20200309.csv", header=TRUE, sep=",", na.string="NA", as.is=TRUE) #reads in the data files
-model <-lm(mVTris ~ TTris, data=Calib.Data) #runs a linear regression of mV as a function of temperature
-coe <- coef(model) #extracts the coeffecients
-R2<-summary(model)$r.squared
-plot(mVTris ~ TTris, data=Calib.Data)
-abline(lm(mVTris ~ TTris, data=Calib.Data))
-legend('topleft', legend = bquote(R^2 == .(format(R2, digits = 3))), bty='n')
